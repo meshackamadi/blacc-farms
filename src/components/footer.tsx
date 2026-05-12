22b46b30b630c6e9684849735e4412b460a0ff4c
Sydneyjnr <Sydneychristopherjnr@gmail.com>
2026-05-12 09:30:25 +0100

    added linting

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { FaLeaf } from "react-icons/fa";
 import {
-  Sprout,
   ChevronDown,
   Twitter,
   Github,
